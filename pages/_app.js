911ff0ca363eb12bbb2af6eb80db0d0b7385b27d
AhmedMAmar <ahmedmedamar@gmail.com>
2022-01-22 23:39:44 +0000

    second

--- pages/_app.js
+++ pages/_app.js
@@ -18,7 +18,7 @@ function MyApp({ Component, pageProps }) {
     NProgress.done();
   });
   return (
-  <UserProvider>
+  <>
     <Head>
     <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/nprogress/0.2.0/nprogress.min.css' integrity='sha512-42kB9yDlYiCEfx2xVwq0q7hT4uf26FUgSIZBK8uiaEnTdShXjwr8Ip1V4xGJMg3mHkUt9nNuTDxunHF0/EgxLQ==' crossOrigin='anonymous' referrerPolicy='no-referrer' />
     </Head>
@@ -27,7 +27,7 @@ function MyApp({ Component, pageProps }) {
         <Component {...pageProps} />
       </Layout>
     </ChakraProvider>
-  </UserProvider>
+  </>
   
   )
 }
